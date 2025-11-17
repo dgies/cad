b1b966a56d349883511d88fc07beb9b8ef9647ea
dgies <dgies@dgies.com>
2025-11-16 23:11:43 -0800

    Add signature to right wing and create left jaw + tooth Still need some fine-tuning

--- Umbrella_Hook/Umbrella Hook v4.py
+++ Umbrella_Hook/Umbrella Hook v4.py
@@ -30,7 +30,7 @@
 #
 
 # +
-from math import sin,cos,tan, asin,acos, atan, radians, floor
+from math import sin,cos,tan, asin,acos, atan, radians, floor, degrees, sqrt
 from datetime import datetime
 from types import SimpleNamespace
 
@@ -64,11 +64,21 @@ m.reach = m.hook_length*cos(radians(m.beam_angle))
 m.body_thickness_far = 8
 m.body_thickness_near = m.body_thickness_far + 0.5*m.drop
 m.wing_thickness = 8
-m.wing_drop_near = m.beam_height * 1.5
+m.wing_drop_near = m.beam_height * 1.333
 m.wing_drop_far = m.beam_height * 0.6667
 m.wing_height_near = m.body_thickness_near + m.wing_drop_near
 m.wing_height_far  = m.body_thickness_far + m.wing_drop_far
 m.edge_fillet = 0.5
+m.wing_drop = m.wing_drop_near - m.wing_drop_far
+m.wing_hyp = sqrt(m.wing_drop**2 + m.reach**2)
+# m.stylish_angle = degrees(asin(m.wing_drop / m.wing_hyp))
+
+m.h_ext = 12
+m.scale_jaw = (m.reach - m.h_ext) / m.reach
+
+
+m.jaw_drop = (m.wing_drop_near + m.drop - m.wing_drop_far)
+m.stylish_angle = degrees(atan(m.jaw_drop / m.reach))
 
 
 print(f"Building from model {m}")
@@ -119,7 +129,19 @@ right_wing = (
     .polarLine(m.wing_height_far, 90) # up
     .close()
 
-).extrude(m.wing_thickness).edges(">(0, -1, 1)").fillet(m.edge_fillet).edges(">(0, 1, 1)").fillet(m.edge_fillet).edges(">(1, 0, 1)").fillet(m.edge_fillet)
+)
+
+right_wing = (
+    right_wing
+    .extrude(m.wing_thickness)
+    .edges(">(0, -1, 1)")
+    .fillet(m.edge_fillet)
+    .edges(">(0, 1, 1)")
+    .fillet(m.edge_fillet)
+    # .edges(">(1, 0, 1)")
+    # .fillet(m.edge_fillet)
+)
+    
 replay(right_wing)
 # Mirror in YZ plane before finishing on right wing
 left_wing = right_wing.mirror(mirrorPlane="YZ", basePointVector=(0, 0, -30))
@@ -139,29 +161,63 @@ signed_right_wing = (
         .text(f"DFG {str(datetime.now().year)}", fontsize=fontsize, distance=depth, fontPath="Arial.ttf",
          halign="right", valign="top", kind="bold")
 )
+signed_right_wing = (
+    signed_right_wing
+    .edges(">(0, -1, -1)")
+    .fillet(m.edge_fillet)
+    .edges(">(0, 1, -1)")
+    .fillet(m.edge_fillet)
+    .edges(">(1, -1, 0)")
+    .fillet(m.edge_fillet)
+)
+
 replay(signed_right_wing)
 
 # +
+# make left wing from right+jaw
+
+
 left_wing = (
-    left_wing.faces("<Z").workplane(centerOption="CenterOfMass")
-    .box(2,2,2)
+    right_wing
+    .mirror(mirrorPlane="YZ", basePointVector=(0, 0, -30))
+    # select left face bottom corner
+    .faces("<X").vertices("<Z").workplane(centerOption="CenterOfMass")
+    # draw the jaw with line segments
+    .line(-scale_jaw*m.reach, -scale_jaw*(m.wing_drop_near + m.drop - m.wing_drop_far)) # diag to far
+    .polarLine(m.h_ext, -180) # jaw out
+    .polarLine(m.wing_thickness, 90) # up    
+    .polarLine(m.h_ext, 0) # jaw back
+    .polarLine(0.35*scale_jaw*m.reach, m.stylish_angle)
+    .polarLine(0.35*scale_jaw*m.reach, 55)
+    .polarLine(0.05*scale_jaw*m.reach, 90)
+    .polarLine(0.35*scale_jaw*m.reach, 125)
+    .close()
+).extrude(-m.wing_thickness)
+
+# fillet completed faces
+left_wing = (
+    left_wing
+    .edges(">( -1, -0.2, 1)").fillet(m.edge_fillet) #
+    .faces(">(0,-1, -1)").fillet(m.edge_fillet) # main jaw bottom
+    .faces(">(0, 0, -1)").fillet(m.edge_fillet) # jaw ext bottom
+    # .edges(">( 0, -1, -1)").fillet(m.edge_fillet) # near  bottom
+    # .edges(">( 0,  1, -1)").fillet(m.edge_fillet) # far bottom
+    # .edges(">(-1, -0, -1)").fillet(m.edge_fillet) # bottom-far-left (interfere tooth)
+)
+
+# build the tooth
+left_wing = (
+    left_wing
+    .faces("+Z").workplane(centerOption="CenterOfMass")
+    .center(0,2)
+    .rect(8,8)
+    .workplane(offset=12)
+    .center(0,-2)
+    .rect(4,2)
+    .loft(combine=True)
 )
 
 replay(left_wing)
-# -
-
-# ## Make the jaw
-# ```
-# Select -X face of left wing
-# Select -Y,-Z vertex
-#
-# Sketch jaw...
-# Extrude wing_thickness
-# Sketch bottom jaw...
-# Extrude wing_thickness
-# Make jaw extension (as solid?)
-# Make tooth
-# ```
 
 # +
 
